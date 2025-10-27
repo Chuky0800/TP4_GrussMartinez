78667482bd722bd601f785cc31ff2326a2477d7e
Chuky <jmartinezhaarth@itba.edu.ar>
2025-10-27 15:39:19 -0300

    TP5: Archivos de las consignas 16, 17 y 18

--- 2.18_InfoMed_TP4_GrussMartinez.sql
+++
@@ -1,5 +0,0 @@
-SELECT m.nombre, count(DISTINCT c.id_paciente) AS "Cantidad de Pacientes" 
-FROM consultas C
-JOIN medicos m ON m.id_medico = C.id_medico
-GROUP BY m.nombre
-ORDER BY count(DISTINCT c.id_paciente) DESC;
